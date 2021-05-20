c9b58c031c0a6fc33a6028282d62a12f3145eeb3
Sergei Lymar <redgooseman55@gmail.com>
2021-05-20 10:11:13 +0300

    Edit controller

--- src/main/java/ru/akvine/PopulationRegistry/controllers/IndexController.java
+++ src/main/java/ru/akvine/PopulationRegistry/controllers/IndexController.java
@@ -1,5 +1,11 @@
 package ru.akvine.PopulationRegistry.controllers;
 
+import ru.akvine.PopulationRegistry.service.PersonService;
+import ru.akvine.PopulationRegistry.service.impl.PersonServiceImpl;
+import ru.akvine.PopulationRegistry.models.Person;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
@@ -8,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
+	private PersonService personService = new PersonServiceImpl();
+	
 	
 	@RequestMapping("/index")
 	public String getRedirect() {
@@ -16,9 +24,11 @@ public class IndexController {
 	
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String getList(Model model) {
-		System.out.println("Hello, controller!");
+		List<Person> people = personService.getPopulationList();
+		model.addAttribute("people", people);
 		
-		model.addAttribute("path", "hello world");
+		System.out.println(people);
+		
 		return "list";
 	}
 }
--- src/main/java/ru/akvine/PopulationRegistry/repository/impl/PersonRepositoryImpl.java
+++ src/main/java/ru/akvine/PopulationRegistry/repository/impl/PersonRepositoryImpl.java
@@ -1,5 +1,9 @@
 package ru.akvine.PopulationRegistry.repository.impl;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 import ru.akvine.PopulationRegistry.models.Person;
 import ru.akvine.PopulationRegistry.repository.PersonRepository;
@@ -12,36 +16,45 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class PersonRepositoryImpl implements PersonRepository {
 	private static Logger logger = LoggerFactory.getLogger(Person.class);
+	private static final AtomicInteger AUTO_ID = new AtomicInteger(0);
+	private static Map<Integer, Person> people = new HashMap<>();
+	
+	
+	static {
+		Person human = new Person();
+		human.setId(AUTO_ID.getAndIncrement());
+		human.setAge(15);
+		human.setBirthDate("2001-03-07");
+		human.setPostal("141301");
+		
+		people.put(human.getId(), human);
+	}
 	
 	
 	@Override
 	public List<Person> getPopulationList() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<>(people.values());
 	}
 
 	@Override
 	public void addPerson(Person person) {
-		// TODO Auto-generated method stub
-		
+		person.setId(AUTO_ID.getAndIncrement());
+		people.put(person.getId(), person);
 	}
 
 	@Override
 	public void deletePerson(int id) {
-		// TODO Auto-generated method stub
-		
+		people.remove(id);
 	}
 
 	@Override
 	public void editPerson(Person person) {
-		// TODO Auto-generated method stub
-		
+		people.put(person.getId(), person);
 	}
 
 	@Override
 	public Person getById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return people.get(id);
 	}
 	
 }
